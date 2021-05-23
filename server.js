ce0e6118aec82225f86b4d00a91912ce744bb1c5
Rohit Bhaskar <bhaskar.rohit.work@gmail.com>
2021-05-23 22:43:14 +0530

    started off with kendo's 'bottom navigation' example and set the basic router and pages

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express(),
       bodyParser = require("body-parser");
       port = 3080;
 
+//allow OPTIONS on all resources
+app.options('*', cors())
+
 const users = [];
+
+const heroes = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 app.use(bodyParser.json());
 app.use(express.static(process.cwd()+"/application/dist/angular-nodejs-example/"));
@@ -17,6 +35,19 @@ app.post('/api/user', (req, res) => {
   users.push(user);
   res.json("user addedd");
 });
+
+app.get('/api/heroes', cors(), (req, res) => {
+  res.json(heroes);
+});
+
+app.get('/api/heroes/:id', cors(), (req, res) => {
+  res.json(heroes.find(hero => hero.id == parseInt(req.params.id)));
+});
+
+app.put('/api/heroes', cors(), (req, res) => {
+  let heroToUpdate = heroes.find(hero => hero.id == parseInt(req.body.id));
+  heroToUpdate.name = req.body.name;
+})
 
 app.get('/', (req,res) => {
   res.sendFile(process.cwd()+"/application/dist/angular-nodejs-example/index.html")
